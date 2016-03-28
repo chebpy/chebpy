7aa5cd486735883312aa4dd67c5ea35a2f91acab
Mark Richardson <mrichardson82@gmail.com>
2016-03-28 15:08:01 +0100

    Adding bndfun.py, classicfun.py and fun.py files to bring the architecture closer to Matlab Chebfun v5.

---
+++ pyfun/bndfun.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Placeholder class
+"""
+
+from __future__ import division
+
+from pyfun.classicfun import ClassicFun
+
+class BndFun(ClassicFun):
+    
+    """Class to reprepsent functions on finite intervals [a,b]"""
+    pass
---
+++ pyfun/classicfun.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Placeholder class
+"""
+
+from __future__ import division
+
+from abc import ABCMeta
+
+from pyfun.fun import Fun
+
+class ClassicFun(Fun):
+    __metaclass__ = ABCMeta
