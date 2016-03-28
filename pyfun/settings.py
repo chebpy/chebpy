f8595581a26f019fae0b2089ed645c352ff6394c
Mark Richardson <mrichardson82@gmail.com>
2016-03-28 17:13:55 +0100

    Adding a default domain and tech properties to the DefaultPrefs class in pyfun/settings.

--- pyfun/settings.py
+++ pyfun/settings.py
@@ -5,8 +5,10 @@ Created on Mon Mar 28 15:21:22 2016
 @author: mark
 """
 
+from numpy import array
 from numpy import finfo
 
 class DefaultPrefs():
     eps  = finfo(float).eps
     tech = "ChebTech2"
+    domain = array([-1., 1.])
