83ce2dd81f3b0704d681b3b12656091b24f7ae73
Mark Richardson <mrichardson82@gmail.com>
2016-06-05 19:09:04 +0100

    Reworking user-facing constructor chebfun(...)

--- chebpy/core/ui.py
+++ chebpy/core/ui.py
@@ -5,17 +5,32 @@
 from chebpy.core.chebfun import Chebfun
 from chebpy.core.settings import DefaultPrefs
 
-# Matlab-Chebfun style constructor
-def chebfun(f, domain=DefaultPrefs.domain, n=None):
+
+def chebfun(f=None, domain=None, n=None):
+    """Chebfun constructor
+    """
+    # chebfun()
+    if f is None:
+        return Chebfun.initempty()
+
+    domain = DefaultPrefs.domain if domain is None else domain
+
+    # chebfun(lambda x: f(x), ... )
     if hasattr(f, "__call__"):
-        f = f
-    elif isinstance(f, str):
-        if len(f) is 1 and f.isalpha():
-            f = lambda x: x
-        else:
-            raise ValueError(f)
-    else:
+        return _initfun(f, domain, n)
+
+     # chebfun('x', ... )
+    if isinstance(f, str) and len(f) is 1 and f.isalpha():
+        return _initfun(lambda x: x, domain, n)
+
+    try:
+        # chebfun(3.14, ... ), chebfun('3.14', ... )
+        return Chebfun.initconst(float(f), domain)
+    except:
         raise ValueError(f)
+
+
+def _initfun(f, domain, n):
     if n is None:
         return Chebfun.initfun_adaptive(f, domain)
     else:
