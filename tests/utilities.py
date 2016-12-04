9d74900fc5cad6a5d996d85e5d2b4846f56a0621
Mark Richardson <mrichardson82@gmail.com>
2016-12-04 11:31:11 +0000

    Simplifying imports

--- tests/utilities.py
+++ tests/utilities.py
@@ -1,25 +1,14 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 22 15:12:43 2016
 
-@author: mark.richardson
-"""
-from numpy import inf
-from numpy import sin
-from numpy import cos
-from numpy import exp
-from numpy import log
-from numpy import finfo
+import numpy as np
 
-from numpy.linalg import norm
-
-eps = finfo(float).eps
+eps = np.finfo(float).eps
 
 def infnorm(x):
-    return norm(x, inf)
+    return np.linalg.norm(x, np.inf)
 
 def scaled_tol(n):
-    tol = 5e1*eps if n < 20 else log(n)**2.5*eps
+    tol = 5e1*eps if n < 20 else np.log(n)**2.5*eps
     return tol
 
 # bespoke test generators
@@ -37,13 +26,13 @@ fun_details = [
     #  Matlab chebfun adaptive degree on [-1,1],
     #  Any roots on the real line?
     # )
-    (lambda x: x**3 + x**2 + x + 1.1, "poly3(x)",        4, True),
-    (lambda x: exp(x),                "exp(x)",         15, False),
-    (lambda x: sin(x),                "sin(x)",         14, True),
-    (lambda x: .2+.1*sin(x),          "(.2+.1*sin(x))", 14, False),
-    (lambda x: cos(20*x),             "cos(20x)",       51, True),
-    (lambda x: 0.*x+1.,               "constfun",        1, False),
-    (lambda x: 0.*x,                  "zerofun",         1, True),
+    (lambda x: x**3 + x**2 + x + 1.1, 'poly3(x)',        4, True),
+    (lambda x: np.exp(x),             'exp(x)',         15, False),
+    (lambda x: np.sin(x),             'sin(x)',         14, True),
+    (lambda x: .2+.1*np.sin(x),       '(.2+.1*sin(x))', 14, False),
+    (lambda x: np.cos(20*x),          'cos(20x)',       51, True),
+    (lambda x: 0.*x+1.,               'constfun',        1, False),
+    (lambda x: 0.*x,                  'zerofun',         1, True),
 ]
 for k, items in enumerate(fun_details):
     fun = items[0]
