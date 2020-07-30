d02c70a7ca5b60f9bc2301b6847e2222cf5735f0
Jan Niklas Rose <janniklas.rose@gmail.com>
2020-07-30 20:57:17 +0200

    Refactor settings.py
    IDEs don't like when class attributes are assigned dynamically.
    Now, UserPrefs has the defaults hard-coded, and the backup class
    DefaultPrefs (for resetting UserPrefs if needed) is created
    dynamically instead.

--- chebpy/core/settings.py
+++ chebpy/core/settings.py
@@ -3,33 +3,30 @@
 import numpy as np
 
 
-def default_names(cls):
-    return [n for n in cls.__dict__.keys() if n[0] != "_"]
-
-def default_values(cls):
-    return [cls.__dict__[k] for k in default_names(cls)]
-
-def default_prefs(cls):
-    prefs = dict()
-    for key, val in zip(default_names(cls), default_values(cls)):
-        prefs[key] = val
-    return prefs
-
 class DefaultPrefs():
-    eps = np.finfo(float).eps
-    tech = "Chebtech2"
-    domain = np.array([-1., 1.])
-    N_plot = 2001
+    pass
+
 
 class UserPrefs():
     def __init__(self):
-        for name, val in default_prefs(DefaultPrefs).items():
-            setattr(self, name, val)
+        self.eps = np.finfo(float).eps
+        self.tech = "Chebtech2"
+        self.domain = np.array([-1., 1.])
+        self.N_plot = 2001
     def reset(self, *names):
-        # only reset DefaultPrefs, in case user set their own attributes
-        names = default_names(DefaultPrefs) if len(names) == 0 else names
+        """Reset default preferences.
+        `.reset()` resets all preferences to the DefaultPrefs state
+        `.reset(*names)` resets only the selected ones.
+        This leaves additional user-added prefs untouched."""
+        if len(names) == 0:
+            names = DefaultPrefs.__dict__.keys()
         for name in names:
             self.__setattr__(name, DefaultPrefs.__dict__[name])
 
-defaultPrefs = DefaultPrefs()
 userPrefs = UserPrefs()
+
+for name, val in userPrefs.__dict__.items():
+    if name[0] != "_":
+        setattr(DefaultPrefs, name, val)
+
+defaultPrefs = DefaultPrefs()
