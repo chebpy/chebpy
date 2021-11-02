69d4c206ec1a6b5f263846bb23ca42d63b1f2e9d
Jan N Rose <janniklas.rose@gmail.com>
2021-11-02 22:13:56 +0100

    Apply black format to tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,31 +3,33 @@ import unittest
 from chebpy import UserPreferences
 from chebpy.core.settings import _preferences
 
+
 def userPref(name):
     """Get the current preference value for name.
     This is how chebpy core modules access these."""
     return getattr(_preferences, name)
 
-class Settings(unittest.TestCase):
 
+class Settings(unittest.TestCase):
     def test_update_pref(self):
         eps_new = 1e-3
-        eps_old = userPref('eps')
+        eps_old = userPref("eps")
         with UserPreferences() as prefs:
             prefs.eps = eps_new
-            self.assertEqual(eps_new, userPref('eps'))
-        self.assertEqual(eps_old, userPref('eps'))
+            self.assertEqual(eps_new, userPref("eps"))
+        self.assertEqual(eps_old, userPref("eps"))
 
     def test_reset(self):
         def change(reset_named=False):
             prefs = UserPreferences()
             prefs.eps = 99
             if reset_named:
-                prefs.reset('eps')
+                prefs.reset("eps")
             else:
                 prefs.reset()
-        eps_bak = userPref('eps')
+
+        eps_bak = userPref("eps")
         change(False)
-        self.assertEqual(eps_bak, userPref('eps'))
+        self.assertEqual(eps_bak, userPref("eps"))
         change(True)
-        self.assertEqual(eps_bak, userPref('eps'))
+        self.assertEqual(eps_bak, userPref("eps"))
--- tests/test_ui.py
+++ tests/test_ui.py
@@ -6,60 +6,60 @@ import numpy as np
 from chebpy import chebfun, pwc
 from chebpy.core.settings import DefaultPreferences
 
-class Constructors(unittest.TestCase):
 
+class Constructors(unittest.TestCase):
     def test_chebfun_null_args(self):
         self.assertTrue(chebfun().isempty)
-        
+
     def test_chebfun_callable(self):
         n = 100
-        d = np.array([-2,0,1])
+        d = np.array([-2, 0, 1])
         f1 = chebfun(np.sin)
         f2 = chebfun(np.sin, d)
         f3 = chebfun(np.sin, n=n)
         f4 = chebfun(np.sin, d, n)
-        
+
         # check domains
-        self.assertTrue(f1.domain==DefaultPreferences.domain)
-        self.assertTrue(f2.domain==d)
-        self.assertTrue(f3.domain==DefaultPreferences.domain)
-        self.assertTrue(f4.domain==d)
-        
+        self.assertTrue(f1.domain == DefaultPreferences.domain)
+        self.assertTrue(f2.domain == d)
+        self.assertTrue(f3.domain == DefaultPreferences.domain)
+        self.assertTrue(f4.domain == d)
+
         # check lengths of f3 and f4
         self.assertEqual(f3.funs[0].size, n)
-        self.assertTrue(np.all([fun.size==n for fun in f4]))
+        self.assertTrue(np.all([fun.size == n for fun in f4]))
 
     def test_chebfun_alphanum_char(self):
         n = 100
-        d = np.array([-2,0,1])
-        f1 = chebfun('x')
-        f2 = chebfun('y', d)
-        f3 = chebfun('z', n=n)
-        f4 = chebfun('a', d, n)
-        
+        d = np.array([-2, 0, 1])
+        f1 = chebfun("x")
+        f2 = chebfun("y", d)
+        f3 = chebfun("z", n=n)
+        f4 = chebfun("a", d, n)
+
         # check domains
-        self.assertTrue(f1.domain==DefaultPreferences.domain)
-        self.assertTrue(f2.domain==d)
-        self.assertTrue(f3.domain==DefaultPreferences.domain)
-        self.assertTrue(f4.domain==d)
-        
+        self.assertTrue(f1.domain == DefaultPreferences.domain)
+        self.assertTrue(f2.domain == d)
+        self.assertTrue(f3.domain == DefaultPreferences.domain)
+        self.assertTrue(f4.domain == d)
+
         # check lengths of f3 and f4
         self.assertEqual(np.sum([fun.size for fun in f3]), n)
-        self.assertTrue(np.all([fun.size==n for fun in f4]))
-        
+        self.assertTrue(np.all([fun.size == n for fun in f4]))
+
     def test_chebfun_float_arg(self):
-        d = np.array([-2,0,1])
+        d = np.array([-2, 0, 1])
         f1 = chebfun(3.14)
-        f2 = chebfun('3.14')
+        f2 = chebfun("3.14")
         f3 = chebfun(2.72, d)
-        f4 = chebfun('2.72', d)
-        
+        f4 = chebfun("2.72", d)
+
         # check domains
-        self.assertTrue(f1.domain==DefaultPreferences.domain)
-        self.assertTrue(f2.domain==DefaultPreferences.domain)
-        self.assertTrue(f3.domain==d)
-        self.assertTrue(f4.domain==d)
-        
+        self.assertTrue(f1.domain == DefaultPreferences.domain)
+        self.assertTrue(f2.domain == DefaultPreferences.domain)
+        self.assertTrue(f3.domain == d)
+        self.assertTrue(f4.domain == d)
+
         # check all are constant
         self.assertTrue(f1.isconst)
         self.assertTrue(f2.isconst)
@@ -67,19 +67,19 @@ class Constructors(unittest.TestCase):
         self.assertTrue(f4.isconst)
 
     def test_chebfun_raises(self):
-        self.assertRaises(ValueError, chebfun, 'asdfasdf')
+        self.assertRaises(ValueError, chebfun, "asdfasdf")
 
     def test_pwc(self):
-        dom = [-1,0,1]
-        vals = [0,1]
+        dom = [-1, 0, 1]
+        vals = [0, 1]
         f = pwc(dom, vals)
         self.assertEqual(f.funs.size, 2)
-        for fun, val in zip(f,vals):
+        for fun, val in zip(f, vals):
             self.assertTrue(fun.isconst)
             self.assertEqual(fun.coeffs[0], val)
 
-class Pickling(unittest.TestCase):
 
+class Pickling(unittest.TestCase):
     def setUp(self):
         self.f0 = chebfun(np.sin, [-2, 0, 1])
         self.f1 = pickle.loads(pickle.dumps(self.f0))
@@ -88,6 +88,6 @@ class Pickling(unittest.TestCase):
         x = -1
         self.assertEqual(self.f0(x), self.f1(x))
 
-    #TODO: implement test for equality once objects can be compared
-    #def test_equality(self):
-        #self.assertEqual(vars(self.f0), vars(self.f1))
+    # TODO: implement test for equality once objects can be compared
+    # def test_equality(self):
+    # self.assertEqual(vars(self.f0), vars(self.f1))
