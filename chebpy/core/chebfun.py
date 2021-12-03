chebpy/chebpy

commit 7089e8dbb493287badf4bed7085323f950e8b3f9

Remove bare try-except blocks

chebpy/core/chebfun.py
@@ -170,11 +170,8 @@ class Chebfun:
         simplify step, since at the Tech-level these operations are are defined
         such that there is no change in the number of coefficients.
         """
-        try:
-            if f.isempty:
-                return f
-        except:
-            pass
+        if hasattr(f, "isempty"):
+            return f
         if np.isscalar(f):
             chbfn1 = self
             chbfn2 = f * np.ones(self.funs.size)

chebpy/api.py
@@ -28,8 +28,8 @@ def chebfun(f=None, domain=None, n=None):
     try:
         # chebfun(3.14, ... ), chebfun('3.14', ... )
         return Chebfun.initconst(float(f), domain)
-    except:
-        raise ValueError(f)
+    except (OverflowError, ValueError):
+        raise ValueError(f"Unable to construct const function from {{{f}}}")
 
 
 def pwc(domain=[-1, 0, 1], values=[0, 1]):

chebpy/core/decorators.py
@@ -87,9 +87,7 @@ def float_argument(f):
         xx = np.array([x]) if np.isscalar(x) else np.array(x)
         # discern between the array(0.1) and array([0.1]) cases
         if xx.size == 1:
-            try:
-                xx[0]
-            except:
+            if xx.ndim == 0:
                 xx = np.array([xx])
         args = list(args)
         args[0] = xx

commit 5b3f0fd29562c81efe431bfa269bca1fd4a6d1e1

Add missing test for empty-ness in Chebfun
Fixes error introduced in 7089e8d

chebpy/core/chebfun.py
@@ -170,7 +170,7 @@ class Chebfun:
         simplify step, since at the Tech-level these operations are are defined
         such that there is no change in the number of coefficients.
         """
-        if hasattr(f, "isempty"):
+        if hasattr(f, "isempty") and f.isempty:
             return f
         if np.isscalar(f):
             chbfn1 = self