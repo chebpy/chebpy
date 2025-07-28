3aa490aa41211ebab2bbb20ddb105d07216f129f
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-07-28 07:35:30 +0200

    towards full pytest

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,6 +1,7 @@
 """Unit-tests for pyfun/utilities.py"""
 
-import unittest
+import pytest
+from typing import Any, Callable, List, Tuple, Union
 
 import numpy as np
 
@@ -8,7 +9,7 @@ from chebpy.core.settings import DefaultPreferences
 from chebpy.core.chebtech import Chebtech2
 from chebpy.core.algorithms import bary, clenshaw, coeffmult
 
-from .utilities import testfunctions, scaled_tol, infNormLessThanTol, infnorm
+from .utilities import testfunctions, scaled_tol, inf_norm_less_than_tol, infnorm
 
 # aliases
 pi = np.pi
@@ -24,74 +25,126 @@ np.random.seed(0)
 np.seterr(divide="ignore", invalid="ignore")
 
 
-class Evaluation(unittest.TestCase):
-    """Tests for the Barycentric formula and Clenshaw algorithm"""
+@pytest.fixture
+def evaluation_fixtures() -> dict[str, Any]:
+    """Create fixtures for testing evaluation algorithms.
 
-    def setUp(self):
-        npts = 15
-        self.xk = Chebtech2._chebpts(npts)
-        self.vk = Chebtech2._barywts(npts)
-        self.fk = np.random.rand(npts)
-        self.ak = np.random.rand(11)
-        self.xx = -1 + 2 * np.random.rand(9)
-        self.pts = -1 + 2 * np.random.rand(1001)
+    Returns
+    -------
+    dict[str, Any]
+        Dictionary containing test fixtures for evaluation
+    """
+    npts = 15
+    xk = Chebtech2._chebpts(npts)
+    vk = Chebtech2._barywts(npts)
+    fk = np.random.rand(npts)
+    ak = np.random.rand(11)
+    xx = -1 + 2 * np.random.rand(9)
+    pts = -1 + 2 * np.random.rand(1001)
+    return {"xk": xk, "vk": vk, "fk": fk, "ak": ak, "xx": xx, "pts": pts}
 
-    # check an empty array is returned whenever either or both of the first
-    # two arguments are themselves empty arrays
-    def test_bary__empty(self):
-        null = (None, None)
-        self.assertEqual(bary(np.array([]), np.array([]), *null).size, 0)
-        self.assertEqual(bary(np.array([0.1]), np.array([]), *null).size, 0)
-        self.assertEqual(bary(np.array([]), np.array([0.1]), *null).size, 0)
-        self.assertEqual(bary(self.pts, np.array([]), *null).size, 0)
-        self.assertEqual(bary(np.array([]), self.pts, *null).size, 0)
-        self.assertNotEqual(bary(np.array([0.1]), np.array([0.1]), *null).size, 0)
 
-    def test_clenshaw__empty(self):
-        self.assertEqual(clenshaw(np.array([]), np.array([])).size, 0)
-        self.assertEqual(clenshaw(np.array([]), np.array([1.0])).size, 0)
-        self.assertEqual(clenshaw(np.array([1.0]), np.array([])).size, 0)
-        self.assertEqual(clenshaw(self.pts, np.array([])).size, 0)
-        self.assertEqual(clenshaw(np.array([]), self.pts).size, 0)
-        self.assertNotEqual(clenshaw(np.array([0.1]), np.array([0.1])).size, 0)
+# check an empty array is returned whenever either or both of the first
+# two arguments are themselves empty arrays
+def test_bary_empty(evaluation_fixtures: dict[str, Any]) -> None:
+    """Test bary function with empty arrays.
 
-    # check that scalars get evaluated to scalars (not arrays)
-    def test_clenshaw__scalar_input(self):
-        for x in self.xx:
-            self.assertTrue(np.isscalar(clenshaw(x, self.ak)))
-        self.assertFalse(np.isscalar(clenshaw(xx, self.ak)))
+    Parameters
+    ----------
+    evaluation_fixtures : dict[str, Any]
+        Dictionary containing test fixtures
+    """
+    pts = evaluation_fixtures["pts"]
+    null = (None, None)
+    assert bary(np.array([]), np.array([]), *null).size == 0
+    assert bary(np.array([0.1]), np.array([]), *null).size == 0
+    assert bary(np.array([]), np.array([0.1]), *null).size == 0
+    assert bary(pts, np.array([]), *null).size == 0
+    assert bary(np.array([]), pts, *null).size == 0
+    assert bary(np.array([0.1]), np.array([0.1]), *null).size != 0
 
-    def test_bary__scalar_input(self):
-        for x in self.xx:
-            self.assertTrue(np.isscalar(bary(x, self.fk, self.xk, self.vk)))
-        self.assertFalse(np.isscalar(bary(xx, self.fk, self.xk, self.vk)))
 
-    # Check that we always get float output for constant Chebtechs, even
-    # when passing in an integer input.
-    # TODO: Move these tests elsewhere?
-    def test_bary__float_output(self):
-        ff = Chebtech2.initconst(1)
-        gg = Chebtech2.initconst(1.0)
-        self.assertTrue(isinstance(ff(0, "bary"), float))
-        self.assertTrue(isinstance(gg(0, "bary"), float))
+def test_clenshaw_empty(evaluation_fixtures: dict[str, Any]) -> None:
+    """Test clenshaw function with empty arrays.
 
-    def test_clenshaw__float_output(self):
-        ff = Chebtech2.initconst(1)
-        gg = Chebtech2.initconst(1.0)
-        self.assertTrue(isinstance(ff(0, "clenshaw"), float))
-        self.assertTrue(isinstance(gg(0, "clenshaw"), float))
+    Parameters
+    ----------
+    evaluation_fixtures : dict[str, Any]
+        Dictionary containing test fixtures
+    """
+    pts = evaluation_fixtures["pts"]
+    assert clenshaw(np.array([]), np.array([])).size == 0
+    assert clenshaw(np.array([]), np.array([1.0])).size == 0
+    assert clenshaw(np.array([1.0]), np.array([])).size == 0
+    assert clenshaw(pts, np.array([])).size == 0
+    assert clenshaw(np.array([]), pts).size == 0
+    assert clenshaw(np.array([0.1]), np.array([0.1])).size != 0
 
-    # Check that we get consistent output from bary and clenshaw
-    # TODO: Move these tests elsewhere?
-    def test_bary_clenshaw_consistency(self):
-        coeffs = np.random.rand(3)
-        evalpts = (0.5, np.array([]), np.array([0.5]), np.array([0.5, 0.6]))
-        for n in range(len(coeffs)):
-            ff = Chebtech2(coeffs[:n])
-            for xx in evalpts:
-                fb = ff(xx, "bary")
-                fc = ff(xx, "clenshaw")
-                self.assertEqual(type(fb), type(fc))
+
+# check that scalars get evaluated to scalars (not arrays)
+def test_clenshaw_scalar_input(evaluation_fixtures: dict[str, Any]) -> None:
+    """Test clenshaw function with scalar inputs.
+
+    Parameters
+    ----------
+    evaluation_fixtures : dict[str, Any]
+        Dictionary containing test fixtures
+    """
+    xx = evaluation_fixtures["xx"]
+    ak = evaluation_fixtures["ak"]
+    for x in xx:
+        assert np.isscalar(clenshaw(x, ak))
+    assert not np.isscalar(clenshaw(xx, ak))
+
+
+def test_bary_scalar_input(evaluation_fixtures: dict[str, Any]) -> None:
+    """Test bary function with scalar inputs.
+
+    Parameters
+    ----------
+    evaluation_fixtures : dict[str, Any]
+        Dictionary containing test fixtures
+    """
+    xx = evaluation_fixtures["xx"]
+    fk = evaluation_fixtures["fk"]
+    xk = evaluation_fixtures["xk"]
+    vk = evaluation_fixtures["vk"]
+    for x in xx:
+        assert np.isscalar(bary(x, fk, xk, vk))
+    assert not np.isscalar(bary(xx, fk, xk, vk))
+
+
+# Check that we always get float output for constant Chebtechs, even
+# when passing in an integer input.
+# TODO: Move these tests elsewhere?
+def test_bary_float_output() -> None:
+    """Test that bary evaluation returns float output for constant Chebtechs."""
+    ff = Chebtech2.initconst(1)
+    gg = Chebtech2.initconst(1.0)
+    assert isinstance(ff(0, "bary"), float)
+    assert isinstance(gg(0, "bary"), float)
+
+
+def test_clenshaw_float_output() -> None:
+    """Test that clenshaw evaluation returns float output for constant Chebtechs."""
+    ff = Chebtech2.initconst(1)
+    gg = Chebtech2.initconst(1.0)
+    assert isinstance(ff(0, "clenshaw"), float)
+    assert isinstance(gg(0, "clenshaw"), float)
+
+
+# Check that we get consistent output from bary and clenshaw
+# TODO: Move these tests elsewhere?
+def test_bary_clenshaw_consistency() -> None:
+    """Test that bary and clenshaw return consistent output types."""
+    coeffs = np.random.rand(3)
+    evalpts = (0.5, np.array([]), np.array([0.5]), np.array([0.5, 0.6]))
+    for n in range(len(coeffs)):
+        ff = Chebtech2(coeffs[:n])
+        for xx in evalpts:
+            fb = ff(xx, "bary")
+            fc = ff(xx, "clenshaw")
+            assert type(fb) == type(fc)
 
 
 evalpts = [np.linspace(-1, 1, int(n)) for n in np.array([1e2, 1e3, 1e4, 1e5])]
@@ -99,7 +152,25 @@ ptsarry = [Chebtech2._chebpts(n) for n in np.array([100, 200])]
 methods = [bary, clenshaw]
 
 
-def evalTester(method, fun, evalpts, chebpts):
+def evalTester(method: Callable, fun: Callable, evalpts: np.ndarray, chebpts: np.ndarray) -> Callable:
+    """Create a test function for evaluating methods.
+
+    Parameters
+    ----------
+    method : Callable
+        The method to test (bary or clenshaw)
+    fun : Callable
+        The function to evaluate
+    evalpts : np.ndarray
+        Points to evaluate the function at
+    chebpts : np.ndarray
+        Chebyshev points
+
+    Returns
+    -------
+    Callable
+        Test function
+    """
     x = evalpts
     xk = chebpts
     fvals = fun(xk)
@@ -118,40 +189,55 @@ def evalTester(method, fun, evalpts, chebpts):
     n = evalpts.size
     tol = tol_multiplier * scaled_tol(n)
 
-    return infNormLessThanTol(a, b, tol)
+    return inf_norm_less_than_tol(a, b, tol)
 
 
+# Dynamically create test functions for each method, function, and evaluation points
 for method in methods:
     for fun, _, _ in testfunctions:
         for j, chebpts in enumerate(ptsarry):
             for k, xx in enumerate(evalpts):
-                testfun = evalTester(method, fun, xx, chebpts)
-                testfun.__name__ = "test_{}_{}_{:02}_{:02}".format(
-                    method.__name__, fun.__name__, j, k
-                )
-                setattr(Evaluation, testfun.__name__, testfun)
+                test_func = evalTester(method, fun, xx, chebpts)
+                test_name = f"test_{method.__name__}_{fun.__name__}_{j:02}_{k:02}"
+                # Add the test function to the global namespace
+                globals()[test_name] = test_func
 
 
-class CoeffMult(unittest.TestCase):
-    def setUp(self):
-        self.f = lambda x: exp(x)
-        self.g = lambda x: cos(x)
-        self.fn = 15
-        self.gn = 15
+@pytest.fixture
+def coeffmult_fixtures() -> dict[str, Any]:
+    """Create fixtures for testing coefficient multiplication.
 
-    def test_coeffmult(self):
-        def h(x):
-            return self.f(x) * self.g(x)
-
-        f, g = self.f, self.g
-        fn, gn = self.fn, self.gn
-        hn = fn + gn - 1
-        fc = Chebtech2.initfun(f, fn).prolong(hn).coeffs
-        gc = Chebtech2.initfun(g, gn).prolong(hn).coeffs
-        hc = coeffmult(fc, gc)
-        HC = Chebtech2.initfun(h, hn).coeffs
-        self.assertLessEqual(infnorm(hc - HC), 2e1 * eps)
+    Returns
+    -------
+    dict[str, Any]
+        Dictionary containing test fixtures for coefficient multiplication
+    """
+    f = lambda x: exp(x)
+    g = lambda x: cos(x)
+    fn = 15
+    gn = 15
+    return {"f": f, "g": g, "fn": fn, "gn": gn}
 
 
-# reset the testsfun variable so it doesn't get picked up by nose
-testfun = None
+def test_coeffmult(coeffmult_fixtures: dict[str, Any]) -> None:
+    """Test coefficient multiplication.
+
+    Parameters
+    ----------
+    coeffmult_fixtures : dict[str, Any]
+        Dictionary containing test fixtures
+    """
+    f = coeffmult_fixtures["f"]
+    g = coeffmult_fixtures["g"]
+    fn = coeffmult_fixtures["fn"]
+    gn = coeffmult_fixtures["gn"]
+
+    def h(x):
+        return f(x) * g(x)
+
+    hn = fn + gn - 1
+    fc = Chebtech2.initfun(f, fn).prolong(hn).coeffs
+    gc = Chebtech2.initfun(g, gn).prolong(hn).coeffs
+    hc = coeffmult(fc, gc)
+    HC = Chebtech2.initfun(h, hn).coeffs
+    assert infnorm(hc - HC) <= 2e1 * eps
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,35 +1,56 @@
-import unittest
+import pytest
 
 from chebpy import UserPreferences
 from chebpy.core.settings import _preferences
 
 
-def userPref(name):
+def userPref(name: str) -> float:
     """Get the current preference value for name.
-    This is how chebpy core modules access these."""
+
+    This is how chebpy core modules access these.
+
+    Parameters
+    ----------
+    name : str
+        Name of the preference to retrieve
+
+    Returns
+    -------
+    float
+        Current value of the preference
+    """
     return getattr(_preferences, name)
 
 
-class Settings(unittest.TestCase):
-    def test_update_pref(self):
-        eps_new = 1e-3
-        eps_old = userPref("eps")
-        with UserPreferences() as prefs:
-            prefs.eps = eps_new
-            self.assertEqual(eps_new, userPref("eps"))
-        self.assertEqual(eps_old, userPref("eps"))
+def test_update_pref() -> None:
+    """Test updating preferences with context manager."""
+    eps_new = 1e-3
+    eps_old = userPref("eps")
+    with UserPreferences() as prefs:
+        prefs.eps = eps_new
+        assert eps_new == userPref("eps")
+    assert eps_old == userPref("eps")
 
-    def test_reset(self):
-        def change(reset_named=False):
-            prefs = UserPreferences()
-            prefs.eps = 99
-            if reset_named:
-                prefs.reset("eps")
-            else:
-                prefs.reset()
 
-        eps_bak = userPref("eps")
-        change(False)
-        self.assertEqual(eps_bak, userPref("eps"))
-        change(True)
-        self.assertEqual(eps_bak, userPref("eps"))
+def test_reset() -> None:
+    """Test resetting preferences."""
+    def change(reset_named: bool = False) -> None:
+        """Helper function to change and reset preferences.
+
+        Parameters
+        ----------
+        reset_named : bool, optional
+            Whether to reset a specific preference or all preferences, by default False
+        """
+        prefs = UserPreferences()
+        prefs.eps = 99
+        if reset_named:
+            prefs.reset("eps")
+        else:
+            prefs.reset()
+
+    eps_bak = userPref("eps")
+    change(False)
+    assert eps_bak == userPref("eps")
+    change(True)
+    assert eps_bak == userPref("eps")
